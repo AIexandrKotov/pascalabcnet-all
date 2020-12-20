5b32bce41691681f0602a0857f8f235acaadbc03
Kotov <44296606+AIexandrKotov@users.noreply.github.com>
2020-12-20 09:32:44 +0500

    new test + attributes test

---
+++ Perfomance tests/GenericMethod_vs_FullMethod.pas
@@ -0,0 +1,26 @@
+﻿uses System;
+uses System.Diagnostics;
+
+function ToEnum<T>(self: string): T; extensionmethod; where T: Enum;
+begin
+  Result := T(Enum.Parse(typeof(T), self));
+end;
+
+begin
+  var sw1 := new Stopwatch();
+  var sw2 := new Stopwatch();
+  sw1.Start();
+  loop 10000000 do
+  begin
+    var __ := ConsoleColor(Enum.Parse(typeof(ConsoleColor), 'Black'));
+  end;
+  sw1.Stop();
+  sw2.Start();
+  loop 10000000 do
+  begin
+    var __ := 'Black'.ToEnum&<ConsoleColor>();
+  end;
+  sw2.Stop();
+  sw1.ElapsedMilliseconds.Println;
+  sw2.ElapsedMilliseconds.Println;
+end.

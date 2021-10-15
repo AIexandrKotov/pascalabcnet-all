120d7c007217076414a496a66216563b1a2fef54
Kotov <44296606+AIexandrKotov@users.noreply.github.com>
2021-10-15 22:07:30 +0300

    Huh

---
+++ Perfomance tests/regex_vs_towords.pas
@@ -0,0 +1,32 @@
+﻿## uses System.Diagnostics;
+
+///тест регексов
+procedure test(re, text: string; iters: integer);
+begin
+  var timer := Stopwatch.StartNew();
+  for var i := 0 to iters - 1 do
+  begin
+    var res := Regex.Match(re, text);
+  end;
+  timer.Stop;
+  timer.ElapsedMilliseconds.Println;
+end;
+
+///тест ту вордс
+procedure test(text: string; iters: integer);
+begin
+  var timer := Stopwatch.StartNew();
+  for var i := 0 to iters - 1 do
+  begin
+    var tw := text.ToWords('; :');
+  end;
+  timer.Stop;
+  timer.ElapsedMilliseconds.Println;
+end;
+
+var re1 := '/:*\;/';
+var text := ' a, b: array[1..10] of integer;';
+var iters := 50000000;
+
+test(re1, text, iters);
+test(text, iters);
---
+++ Xml test/test.pas
@@ -0,0 +1,12 @@
+﻿uses System.Xml;
+uses System.Xml.Linq;
+
+{$reference 'System.Xml.dll'}
+{$reference 'System.Xml.Linq.dll'}
+
+begin
+  var doc := new XmlDocument();
+  doc.LoadXml(ReadAllText('test.xml', Encoding.UTF8));
+  var xRoot := doc.DocumentElement;
+    xRoot.FirstChild.ChildNodes.OfType&<XmlElement>.Select(x -> x.HasChildNodes ? x.ChildNodes.OfType&<XmlElement>.Select(y -> y.GetAttribute('v').ToInteger).Sum + x.GetAttribute('v').ToInteger : x.GetAttribute('v').ToInteger()).ToArray().PrintLines
+end.
